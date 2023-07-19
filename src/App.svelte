0580c6bf037772ca524df8f27acb2d6c374ec2f1
Phoomparin Mano <phoomparin@gmail.com>
2023-07-19 21:37:44 +0700

    feat: tailwind init

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,7 @@
 <main>
-  <h1>YWC Relationship Tester</h1>
+  <div class="flex items-center justify-center w-full min-h-screen px-12">
+    <h1 class="text-center text-2xl lg:text-6xl font-semibold">
+      YWC Relationship Tester
+    </h1>
+  </div>
 </main>
