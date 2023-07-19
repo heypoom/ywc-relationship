05599854883ec8aacf7e7f6f4c45e33ec951a241
Phoomparin Mano <phoomparin@gmail.com>
2023-07-19 21:48:23 +0700

    chore: sveltekit init

--- src/App.svelte
+++
@@ -1,7 +0,0 @@
-<main>
-  <div class="flex items-center justify-center w-full min-h-screen px-12">
-    <h1 class="text-center text-2xl lg:text-6xl font-semibold">
-      YWC Relationship Tester
-    </h1>
-  </div>
-</main>
